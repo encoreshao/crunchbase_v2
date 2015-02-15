e699f1ecd098f2dcb2fb4dc40c3c902e801bd2e6
Ladislav Martincik <ladislav.martincik@gmail.com>
2015-02-15 11:22:19 +0100

    Updating Headquarters to actual API

--- lib/crunchbase/headquarter.rb
+++ lib/crunchbase/headquarter.rb
@@ -4,22 +4,27 @@
 
 module Crunchbase
   class Headquarter < CBEntity
-    
+
     RESOURCE_LIST = 'headquarters'
 
-    attr_reader :type_name, :name, :street_1, :street_2, :city, :city_uuid, :city_path, 
-                :region, :country_code, :created_at, :updated_at
-                
+    attr_reader :type_name, :name, :street_1, :street_2, :postal_code, :city, :city_path,
+                :region, :region_path, :country, :country_path, :latitude, :longitude,
+                :created_at, :updated_at
+
     def initialize(json)
       @type_name      = json['type']
       @name           = json['name']
       @street_1       = json['street_1']
       @street_2       = json['street_2']
+      @postal_code    = json['postal_code']
       @city           = json['city']
-      @city_uuid      = json['city_uuid']
       @city_path      = json['city_path']
       @region         = json['region']
-      @country_code   = json['country_code']
+      @region_path    = json['region_path']
+      @country        = json['country']
+      @country_path   = json['country_path']
+      @latitude       = json['latitude']
+      @longitude      = json['longitude']
       @created_at     = Time.at(json['created_at']).utc
       @updated_at     = Time.at(json['updated_at']).utc
     end
